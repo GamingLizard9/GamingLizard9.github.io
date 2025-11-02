e6c36bba42bca586d675da513a8102675a3e74c2
GamingLizard9 <97365805+GamingLizard9@users.noreply.github.com>
2025-11-02 12:13:04 -0500

    Use Passthrough Image Service to bypass Sharp
    I'm just trying things at this point

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://gaminglizard9.github.io',
+    image: {
+        service: passthroughImageService(),
+    },
 });
