558e9ae9c66470ef74314a0dd7f948cc6e2c62ff
GamingLizard9 <97365805+GamingLizard9@users.noreply.github.com>
2025-11-02 12:04:12 -0500

    Update site URL in astro.config.mjs
    It had the wrong URL, and now it has the right one

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
+    site: 'https://gaminglizard9.github.io',
 });
